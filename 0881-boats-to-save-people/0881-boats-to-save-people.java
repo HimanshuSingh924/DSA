b138c53cffb6cc54b76314640defce83933defae
HimanshuSingh924 <72846461+HimanshuSingh924@users.noreply.github.com>
2026-03-16 14:18:47 +0530

    Time: 20 ms (83.21%), Space: 56.4 MB (86.64%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -1,13 +1,5 @@
 class Solution {
     // HimanshuSingh924
-    static {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-                fw.write("0");
-            } catch (Exception e) {
-            }
-        }));
-    }
     public int numRescueBoats(int[] people, int limit) {
         Arrays.sort(people);
 
@@ -16,17 +8,14 @@ class Solution {
 		
 		int count = 0;
 
-		while (start <= end)
-		{
+		while (start <= end) {
 			int sum = people[start] + people[end];
 
-			if (sum <= limit)
-			{	
+			if (sum <= limit) {	
 				start++;
 				end--;
 			}
-			else
-			{
+			else{
 				end--;
 			}
 
