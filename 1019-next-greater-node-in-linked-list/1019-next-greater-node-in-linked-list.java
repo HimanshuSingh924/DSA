6f6da9ea8a8a455e33c6bb89481487627603980b
HimanshuSingh924 <72846461+HimanshuSingh924@users.noreply.github.com>
2026-05-13 23:48:31 +0530

    Time: 25 ms (50%), Space: 49.6 MB (74.63%) - LeetHub

---
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.java
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[] nextLargerNodes(ListNode head) {
+        int size = size(head);
+        int[] arr = new int[size];
+        ListNode newHead = reverse(head);
+
+        Stack<Integer> st = new Stack<>();
+        st.push(newHead.val);
+
+        ListNode curr = newHead.next;
+        int ptr = size-2;
+
+        while(ptr >= 0){
+            int ele = curr.val;
+            curr = curr.next;
+
+            while(st.size() > 0 && st.peek() <= ele){
+                st.pop();
+            }
+
+            if(st.size() == 0) arr[ptr] = 0; 
+            else arr[ptr] = st.peek();
+
+            // arr[ptr] = (st.size() == 0) ? 0 : st.peek();
+
+            st.push(ele);
+            ptr--;
+        }
+        return arr;
+	}
+
+    private int size(ListNode head){
+        ListNode curr = head;
+        int count = 0;
+        while(curr != null){
+            count++;
+            curr =curr.next;
+        }
+        return count;
+    }
+
+    private ListNode reverse(ListNode head){
+        ListNode curr = head;
+        ListNode pre = null;
+        while(curr != null){
+            ListNode temp = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = temp;
+        }
+        return pre;
+    }
+}
