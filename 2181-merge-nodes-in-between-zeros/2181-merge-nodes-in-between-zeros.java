849591110812550c537c7f9c0623120013413437
HimanshuSingh924 <72846461+HimanshuSingh924@users.noreply.github.com>
2026-05-15 15:43:36 +0530

    Time: 6 ms (72.16%), Space: 260.7 MB (47.48%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
@@ -10,37 +10,61 @@
  */
 class Solution {
     public ListNode mergeNodes(ListNode head) {
-        List<Integer> list = new ArrayList<>();
-		List<Integer> ans = new ArrayList<>();
-		while (head != null)
-		{
-			list.add(head.val);
-			head = head.next;
-		}
+        // List<Integer> list = new ArrayList<>();
+		// List<Integer> ans = new ArrayList<>();
+		// while (head != null)
+		// {
+		// 	list.add(head.val);
+		// 	head = head.next;
+		// }
 
-		int sum = 0;
-		for (int i = 1; i < list.size(); i++)
-		{
-			if (list.get(i) != 0)
-			{
-				sum += list.get(i);
-			}
-			else
-			{
-				ans.add(sum);
-				sum=0;
-			}
-		}
+		// int sum = 0;
+		// for (int i = 1; i < list.size(); i++)
+		// {
+		// 	if (list.get(i) != 0)
+		// 	{
+		// 		sum += list.get(i);
+		// 	}
+		// 	else
+		// 	{
+		// 		ans.add(sum);
+		// 		sum=0;
+		// 	}
+		// }
 		
-		ListNode newHead = new ListNode(ans.get(0));
-		ListNode curr = newHead;
+		// ListNode newHead = new ListNode(ans.get(0));
+		// ListNode curr = newHead;
 
-		for (int i = 1; i < ans.size(); i++)
-		{
-			curr.next = new ListNode(ans.get(i));
-			curr = curr.next;
-		}
+		// for (int i = 1; i < ans.size(); i++)
+		// {
+		// 	curr.next = new ListNode(ans.get(i));
+		// 	curr = curr.next;
+		// }
 
-		return newHead;
+		// return newHead;
+ 
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        int sum = 0;
+
+        head = head.next;
+
+        while(head != null){
+            if(0 != head.val){
+                sum += head.val;
+            }else{
+                ListNode temp = new ListNode(sum);
+
+                curr.next = temp;
+                curr = curr.next;
+
+                sum = 0;
+            }
+
+            head = head.next;
+        }
+
+        return dummy.next;
     }
 }
