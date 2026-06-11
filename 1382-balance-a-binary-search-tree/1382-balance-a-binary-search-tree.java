1763616cef3659f0c6ebf2458646b7ff4f2e5d0b
HimanshuSingh924 <72846461+HimanshuSingh924@users.noreply.github.com>
2026-06-11 23:01:34 +0530

    Time: 2 ms (98.67%), Space: 48.3 MB (40.26%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.java
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.java
@@ -14,31 +14,29 @@
  * }
  */
 class Solution {
-    // List<Integer> inorder = new ArrayList<>();
+    List<Integer> inorder = new ArrayList<>();
 
     public TreeNode balanceBST(TreeNode root) {
-        List<Integer> inorder = new ArrayList<>();
-    
-        inorderTraversal(inorder, root);
-        return buildBalancedBST(inorder, 0, inorder.size() - 1);
+        inorderTraversal(root);
+        return buildBalancedBST(0, inorder.size() - 1);
     }
 
-    private void inorderTraversal(List<Integer> inorder, TreeNode root) {
+    private void inorderTraversal(TreeNode root) {
         if (root == null) return;
 
-        inorderTraversal(inorder, root.left);
+        inorderTraversal(root.left);
         inorder.add(root.val);
-        inorderTraversal(inorder, root.right);
+        inorderTraversal(root.right);
     }
 
-    private TreeNode buildBalancedBST(List<Integer> inorder,int start, int end) {
+    private TreeNode buildBalancedBST(int start, int end) {
         if (start > end) return null;
 
         int mid = start + (end - start) / 2;
         TreeNode root = new TreeNode(inorder.get(mid));
 
-        root.left = buildBalancedBST(inorder, start, mid - 1);
-        root.right = buildBalancedBST(inorder, mid + 1, end);
+        root.left = buildBalancedBST(start, mid - 1);
+        root.right = buildBalancedBST(mid + 1, end);
 
         return root;
     }
